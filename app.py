12bbe0ccc8f277012a477be2c89bc4fcbb8853f4
Beanbiin <mars918196@gmail.com>
2020-05-09 04:53:26 +0800

    template 1

---
+++ app.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, flash, Markup
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, PasswordField, IntegerField, TextField,\
+    FormField, SelectField, FieldList
+from wtforms.validators import DataRequired, Length
+from wtforms.fields import *
+
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+
+
+class ExampleForm(FlaskForm):
+    """An example form that contains all the supported bootstrap style form fields."""
+    date = DateField(description="We'll never share your email with anyone else.")  # add help text with `description`
+    datetime = DateTimeField(render_kw={'placeholder': 'this is placeholder'})  # add HTML attribute with `render_kw`
+    image = FileField(render_kw={'class': 'my-class'})  # add your class
+    option = RadioField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
+    select = SelectField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
+    selectmulti = SelectMultipleField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
+    bio = TextAreaField()
+    title = StringField()
+    secret = PasswordField()
+    remember = BooleanField('Remember me')
+    submit = SubmitField()
+
+
+class HelloForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField()
+
+
+class ButtonForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    submit = SubmitField()
+    delete = SubmitField()
+    cancel = SubmitField()
+
+
+class TelephoneForm(FlaskForm):
+    country_code = IntegerField('Country Code')
+    area_code = IntegerField('Area Code/Exchange')
+    number = TextField('Number')
+
+
+class IMForm(FlaskForm):
+    protocol = SelectField(choices=[('aim', 'AIM'), ('msn', 'MSN')])
+    username = TextField()
+
+
+class ContactForm(FlaskForm):
+    first_name = TextField()
+    last_name = TextField()
+    mobile_phone = FormField(TelephoneForm)
+    office_phone = FormField(TelephoneForm)
+    emails = FieldList(TextField("Email"), min_entries=3)
+    im_accounts = FieldList(FormField(IMForm), min_entries=2)
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def test_form():
+    form = HelloForm()
+    return render_template('form.html', form=form, telephone_form=TelephoneForm(), contact_form=ContactForm(), im_form=IMForm(), button_form=ButtonForm(), example_form=ExampleForm())
+
+
+@app.route('/nav', methods=['GET', 'POST'])
+def test_nav():
+    return render_template('nav.html')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
